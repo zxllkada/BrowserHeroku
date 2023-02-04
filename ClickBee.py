6d8a848f0409897a8782cd4b86e0d9dfc7ff384c
kada <soulayman.kada@gmail.com>
2023-02-04 09:10:54 +0100

    Update ClickBee.py

--- ClickBee.py
+++ ClickBee.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 
 from telethon.tl.functions.messages import GetHistoryRequest
 from telethon.tl.functions.messages import ForwardMessagesRequest
