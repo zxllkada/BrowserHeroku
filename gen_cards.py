71a36973e91c34e0edcb6a07116a20c415e6b3a3
kada <soulayman.kada@gmail.com>
2023-02-04 01:36:02 +0100

    Update gen_cards.py

--- gen_cards.py
+++ gen_cards.py
@@ -29,8 +29,8 @@ headers = {
 Cards_Datails = {}
 
 driver_path = os.environ.get("GOOGLEDRIVER_PATH")
-options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options = webdriver.ChromeOptions()
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
