2ba93a7b65b6caa4f6001010a71420a11a6237a4
kada <soulayman.kada@gmail.com>
2023-02-04 09:10:31 +0100

    Update bypass_faucet.py

--- bypass_faucet.py
+++ bypass_faucet.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
