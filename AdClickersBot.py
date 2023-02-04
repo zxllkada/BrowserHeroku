6e5dca60f6e8983d4ed36f8f49e69ecc63e2f015
kada <soulayman.kada@gmail.com>
2023-02-04 09:11:13 +0100

    Update AdClickersBot.py

--- AdClickersBot.py
+++ AdClickersBot.py
@@ -1,4 +1,4 @@
-# from selenium import webdriver
+from selenium import webdriver
 # from selenium.webdriver.chrome.service import Service
 # from webdriver_manager.chrome import ChromeDriverManager
 
